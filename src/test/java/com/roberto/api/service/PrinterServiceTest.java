bc6a918453ee653ddcfc46d8205df0c6f9e81c12
Thiago Bos Scheid <thiago.scheid@luizalabs.com>
2021-04-19 19:45:25 -0300

    Cobertura de teste

--- src/test/java/com/roberto/api/service/PrinterServiceTest.java
+++ src/test/java/com/roberto/api/service/PrinterServiceTest.java
@@ -1,11 +1,14 @@
 package com.roberto.api.service;
 
+import static org.junit.Assert.assertNotNull;
+import javax.print.PrintServiceLookup;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -29,5 +32,15 @@ public class PrinterServiceTest {
 	@Test
 	public void shouldNotNullService() {
 		Assert.assertNotNull(this.service);
+	}
+
+	@Test
+	public void getPrinterServerSucessTest() {
+		
+		Mockito.mock(PrintServiceLookup.class);			
+		
+		var list = service.getPrinterServer();
+		
+		assertNotNull(list);
 	}
 }
