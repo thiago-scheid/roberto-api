572bc35c0b22a80ff6fa4b41333406f10b3b369a
Thiago Bos Scheid <thiago.scheid@icloud.com>
2021-05-04 00:21:47 +0000

    Update deploy/deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,16 +1,16 @@
-wget https://github.com/luizalabs/teresa/releases/download/v0.31.0/teresa-linux-amd64
-mv ./teresa-linux-amd64 teresa
+wget ""
+mv ./linux-amd64
 
 VERSION=$(mvn -q com.smartcodeltd:release-candidate-maven-plugin:LATEST:version -DoutputTemplate="{{ version }}")​
 
-chmod +x ./teresa
+chmod +x ./all
 
 echo "publish app $2 $VERSION in cluster $1"
-./teresa version
-./teresa config set-cluster server-deploy --server $1 --tls
-./teresa config use-cluster server-deploy
+./all version
+./all config set-cluster server-deploy --server $1 --tls
+./all config use-cluster server-deploy
 
-echo $TERESA_PASSWORD | ./teresa login --user $TERESA_USER
+echo $DEPLOY_PASSWORD | ./all login --user $DEPLOY_USER
 
 mkdir package
 
@@ -26,5 +26,5 @@ ls package -al
 
 echo "app info ..."
 
-./teresa app info $2
-./teresa deploy create package/. --app $2 --no-input --description $VERSION
+./all app info $2
+./all deploy create package/. --app $2 --no-input --description $VERSION
